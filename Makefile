5bf8ecceeb8cf048fd40fe380d34519e868ff15b
Iury Santos <iusantos@c1r7p2.42sp.org.br>
2023-05-09 09:16:59 -0300

    ft_memcmp

--- Makefile
+++ Makefile
@@ -17,7 +17,8 @@ SRC_FILES = ft_putchar_fd.c \
 			ft_bzero.c \
 			ft_memset.c \
 			ft_memcpy.c \
-			ft_memmove.c
+			ft_memmove.c \
+			ft_memcmp.c
 
 OBJ_FILES = $(SRC_FILES:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: iusantos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 09:10:21 by iusantos          #+#    #+#             */
-/*   Updated: 2023/05/08 11:36:49 by iusantos         ###   ########.fr       */
+/*   Updated: 2023/05/09 08:53:24 by iusantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,6 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
