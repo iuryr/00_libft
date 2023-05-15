3e05e8c2ee70a3e7af05b918e13185a4b209b85f
Iury Santos <iusantos@c1r7p2.42sp.org.br>
2023-05-15 13:41:04 -0300

    dealing with case when start > strlen(s)

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: iusantos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 11:22:19 by iusantos          #+#    #+#             */
-/*   Updated: 2023/05/15 11:50:59 by iusantos         ###   ########.fr       */
+/*   Updated: 2023/05/15 13:39:20 by iusantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*substr;
 	char	*cpy_from;
 
+	if (start > ft_strlen(s))
+	{
+		substr = malloc(1);
+		substr[0] = '\0';
+		return (substr);
+	}
 	substr = malloc(len + 1);
 	if (substr == NULL)
 		return (NULL);
