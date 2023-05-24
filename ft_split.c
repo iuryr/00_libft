b9c8d7e1ec201da82a685559fdc9753a51ccbc8e
Iury Santos <iusantos@c1r10p1.42sp.org.br>
2023-05-24 15:02:01 -0300

    fixing word_dup

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: iusantos <iusantos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 15:02:40 by iusantos          #+#    #+#             */
-/*   Updated: 2023/05/24 10:00:33 by iusantos         ###   ########.fr       */
+/*   Updated: 2023/05/24 15:01:39 by iusantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,13 +39,15 @@ static char	*word_dup(const char *str, size_t start, size_t end)
 	size_t	i;
 
 	word = malloc((end - start + 1) * sizeof(char));
+	if (!word)
+		return (0);
 	i = 0;
 	while (i < end - start)
 	{
 		word[i] = str[start + i];
 		i++;
 	}
-	word[end] = '\0';
+	word[i] = '\0';
 	return (word);
 }
 
@@ -74,6 +76,5 @@ char	**ft_split(char const *s, char c)
 			word_start = -1;
 		}
 	}
-	split[j] = NULL;
 	return (split);
 }
