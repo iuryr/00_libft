e68d4ed3f2239449bcf22aca15b035027038c197
Iury Santos <iusantos@c1r10p3.42sp.org.br>
2023-05-25 15:07:21 -0300

    ft_lstlast

--- Makefile
+++ Makefile
@@ -41,7 +41,8 @@ SRC_FILES = ft_putchar_fd.c \
 
 BONUS_FILES = ft_lstnew_bonus.c \
 			  ft_lstadd_front_bonus.c \
-			  ft_lstsize_bonus.c
+			  ft_lstsize_bonus.c \
+			  ft_lstlast_bonus.c
 
 OBJ_FILES = $(SRC_FILES:.c=.o)
 BONUS_OBJ_FILES = $(BONUS_FILES:.c=.o)
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: iusantos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 09:10:21 by iusantos          #+#    #+#             */
-/*   Updated: 2023/05/25 14:43:17 by iusantos         ###   ########.fr       */
+/*   Updated: 2023/05/25 15:00:42 by iusantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,5 +59,6 @@ char	*ft_itoa(int n);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif
