b8bedc9b8646b2c6e61804c3c0c0cd3bc4f7693c
Iury Santos <iusantos@c1r10p3.42sp.org.br>
2023-05-25 18:21:33 -0300

    dealing with NULL in both params

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: iusantos <iusantos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 10:34:41 by iusantos          #+#    #+#             */
-/*   Updated: 2023/05/24 10:34:45 by iusantos         ###   ########.fr       */
+/*   Updated: 2023/05/25 18:15:57 by iusantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
+	if (!dest && !src)
+		return (NULL);
 	i = 0;
 	while (i < n)
 	{
